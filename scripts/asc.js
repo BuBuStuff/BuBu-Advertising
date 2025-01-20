759d577721b6d9b183f616588e59ff06ad6a30d5
BuBu <150358542+BuBuStuff@users.noreply.github.com>
2025-01-20 18:24:05 +0800

    Update asc.js

--- scripts/asc.js
+++ scripts/asc.js
@@ -1,4 +1,4 @@
 var asc = [
   {image: "1.png", url: "https://kamiapp.com"},
-  {image: "2.png", url: "https://google.com/chromeos"},
+  {image: "2.png", url: "https://bubustuff.github.io/ShtOs/"},
 ]
