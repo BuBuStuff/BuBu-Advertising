dbf3aa0cba5900e8347b6cee288ddaf4ca9de1a2
BuBu <150358542+BuBuStuff@users.noreply.github.com>
2025-01-21 07:57:17 +0800

    Update and rename asc_banner.js to asc.js

--- scripts/asc.js
+++ scripts/asc.js
@@ -1,4 +1,8 @@
-var asc = [
+var asc_banner = [
   {image: "1.png", url: "https://kamiapp.com"},
   {image: "2.png", url: "https://bubustuff.github.io/ShtOs/"},
+]
+
+var asc_large_box = [
+  {image: "1.png", url: "https://google.com/"},
 ]
