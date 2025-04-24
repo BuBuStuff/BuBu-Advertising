1ffcd46b869463064c170b0c4669ea382d7188f1
BuBu <150358542+BuBuStuff@users.noreply.github.com>
2025-04-24 11:06:58 +0800

    Update asc.js

--- scripts/asc.js
+++ scripts/asc.js
@@ -1,10 +1,15 @@
 var asc_banner = [
   {image: "1.png", url: "javascript:if (confirm('WARNING: Kami kills.')) {window.open('https://kamiapp.com')} else {void(0);}"},
   {image: "2.png", url: "https://bubustuff.github.io/ShtOs/"},
+  {image: "3.png", url: "https://pap.org.sg/"},
+  {image: "4.png", url: "https://pap.org.sg/"},
 ]
 
 var asc_large_box = [
   {image: "1.png", url: "https://google.com/search?q=How+useless+am+I%3F"},
   {image: "2.png", url: "https://google.com/search?q=Portable+Pee+Bottles"},
   {image: "3.png", url: "https://www.idcpc.org.cn/english2023/"},
+  {image: "4.png", url: "https://pap.org.sg/"},
+  {image: "5.png", url: "https://pap.org.sg/"},
+  {image: "6.png", url: "https://pap.org.sg/"},
 ]
